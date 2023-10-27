fcf0380053f16a672cae0d5faff743d05397f9f1
Daniel Diamand <daniel@diamand.org>
2023-10-27 14:27:27 +0100

    Complete step1

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,10 @@
 package gol
 
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -12,22 +17,99 @@ type distributorChannels struct {
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
+	// Send things down channels to start
+	c.ioCommand <- ioInput
+	c.ioFilename <- fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight)
+
 	// TODO: Create a 2D slice to store the world.
 
-	turn := 0
-
+	world := make([][]byte, p.ImageHeight)
+	for y := 0; y < p.ImageHeight; y++ {
+		world[y] = make([]byte, p.ImageHeight)
+		for x := 0; x < p.ImageHeight; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
 
 	// TODO: Execute all turns of the Game of Life.
+	turn := 0
+	for ; turn < p.Turns; turn++ {
+		world = calculateNextState(p, world)
+	}
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
+	c.events <- FinalTurnComplete{turn, calculateAliveCells(p, world)}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
 	c.events <- StateChange{turn, Quitting}
-	
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+func liveNeighbourCount(y, x int, p Params, world [][]byte) int8 {
+	var count int8 = 0
+	if world[(y+1+p.ImageHeight)%p.ImageHeight][(x+1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	if world[(y+1+p.ImageHeight)%p.ImageHeight][x] == 255 {
+		count++
+	}
+	if world[(y+1+p.ImageHeight)%p.ImageHeight][(x-1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	if world[y][(x+1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	if world[y][(x-1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	if world[(y-1+p.ImageHeight)%p.ImageHeight][(x+1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	if world[(y-1+p.ImageHeight)%p.ImageHeight][x] == 255 {
+		count++
+	}
+	if world[(y-1+p.ImageHeight)%p.ImageHeight][(x-1+p.ImageHeight)%p.ImageHeight] == 255 {
+		count++
+	}
+	return count
+}
+
+//using indexing x,y where 0,0 is top left of board
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for y := 0; y < p.ImageHeight; y++ {
+		newWorld[y] = make([]byte, p.ImageHeight)
+		for x := 0; x < p.ImageHeight; x++ {
+			count := liveNeighbourCount(y, x, p, world)
+			if world[y][x] == 255 { //if cells alive:
+				if count == 2 || count == 3 { //any live cell with two or three live neighbours is unaffected
+					newWorld[y][x] = 255
+				}
+				//any live cell with fewer than two or more than three live neighbours dies
+				//in go slices are initialized to zero, so we don't need to do anything
+			} else { //cells dead
+				if count == 3 { //any dead cell with exactly three live neighbours becomes alive
+					newWorld[y][x] = 255
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	cells := make([]util.Cell, 0)
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageHeight; x++ {
+			if world[y][x] == 255 {
+				cells = append(cells, util.Cell{x, y})
+			}
+		}
+	}
+	return cells
 }
